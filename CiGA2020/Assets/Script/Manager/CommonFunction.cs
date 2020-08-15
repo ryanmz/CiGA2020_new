ed9890ea89619cdd3efad96f7e3d549bdf9f01e3
ryanmz <ryan_qmz@qq.com>
2020-08-15 08:27:39 +0800

    qmz
    update

--- CiGA2020/Assets/Script/Manager/CommonFunction.cs
+++ CiGA2020/Assets/Script/Manager/CommonFunction.cs
@@ -5,4 +5,21 @@ using UnityEngine;
 public class CommonFunction : Singleton<CommonFunction>
 {
 
+    // 改变遮挡关系
+    #region
+    public void ChangeOrder(Collider2D self, Collider2D other)
+    {
+        if (other.transform.position.y < self.transform.position.y)
+        {
+            other.GetComponent<SpriteRenderer>().sortingOrder = 1;
+            self.GetComponent<SpriteRenderer>().sortingOrder = 0;
+        }
+        else
+        {
+            self.GetComponent<SpriteRenderer>().sortingOrder = 1;
+            other.GetComponent<SpriteRenderer>().sortingOrder = 0;
+        }
+
+    }
+    #endregion
 }
